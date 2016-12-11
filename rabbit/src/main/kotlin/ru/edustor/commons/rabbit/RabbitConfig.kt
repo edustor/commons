7ccaa706b2fd057ed2e19f47d7a7ce9880554849
Dmitry Romanov <me@wutiarn.ru>
2016-12-11 06:29:17 +0300

    rabbit: Jackson converter config

--- rabbit/src/main/kotlin/ru/edustor/commons/rabbit/RabbitConfig.kt
+++ rabbit/src/main/kotlin/ru/edustor/commons/rabbit/RabbitConfig.kt
@@ -6,9 +6,10 @@ import org.springframework.amqp.core.TopicExchange
 import org.springframework.amqp.rabbit.annotation.EnableRabbit
 import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory
+import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.amqp.rabbit.retry.RejectAndDontRequeueRecoverer
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -33,10 +34,22 @@ open class RabbitConfig {
         return Binding("rejected.edustor", Binding.DestinationType.QUEUE, "reject.edustor", "#", null)
     }
 
+    @Bean
+    open fun jacksonMessageConverter(): Jackson2JsonMessageConverter {
+        val converter = Jackson2JsonMessageConverter()
+        return converter
+    }
+
     @Autowired fun configureContainer(factory: SimpleRabbitListenerContainerFactory) {
         factory.setAdviceChain(
                 interceptor()
         )
+        factory.setMessageConverter(jacksonMessageConverter())
+    }
+
+    @Autowired
+    fun configureRabbitTemplate(template: RabbitTemplate) {
+        template.messageConverter = jacksonMessageConverter()
     }
 
     @Bean
