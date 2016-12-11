62fc6d5ea0164016057ac3899ff7afa367391e4a
Dmitry Romanov <me@wutiarn.ru>
2016-12-11 06:11:21 +0300

    models: PageRecognizedEvent

---
+++ models/src/main/kotlin/ru/edustor/commons/models/internal/processing/pdf/PageRecognizedEvent.kt
@@ -0,0 +1,13 @@
+package ru.edustor.commons.models.internal.processing.pdf
+
+import java.time.Instant
+
+data class PageRecognizedEvent (
+    val uploadUuid: String,
+    val pageUuid: String,
+    val userId: String,
+    val pageIndex: Int?,
+    val qrUuid: String?,
+    val targetLessonId: String?,
+    val uploadedTimestamp: Instant?
+)
