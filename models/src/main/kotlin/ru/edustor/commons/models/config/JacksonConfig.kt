65f396371cfdea1db630a7af9f405c0bd4ba5370
Dmitry Romanov <me@wutiarn.ru>
2016-12-11 08:56:35 +0300

    models: Disable DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES feature

--- models/src/main/kotlin/ru/edustor/commons/models/config/JacksonConfig.kt
+++ models/src/main/kotlin/ru/edustor/commons/models/config/JacksonConfig.kt
@@ -1,14 +1,19 @@
 package ru.edustor.commons.models.config
 
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 
+@Suppress("SpringKotlinAutowiring")
 @Configuration
 open class JacksonConfig {
     @Autowired
     fun configureObjectMapper(objectMapper: ObjectMapper) {
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
         objectMapper.registerModule(JavaTimeModule())
+        objectMapper.registerKotlinModule()
     }
 }
