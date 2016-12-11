44779225744217228652585d2a6abc4cbb855002
Dmitry Romanov <me@wutiarn.ru>
2016-12-11 06:11:37 +0300

    models: PageRecognizedEvent format

--- models/src/main/kotlin/ru/edustor/commons/models/internal/processing/pdf/PageRecognizedEvent.kt
+++ models/src/main/kotlin/ru/edustor/commons/models/internal/processing/pdf/PageRecognizedEvent.kt
@@ -2,12 +2,12 @@ package ru.edustor.commons.models.internal.processing.pdf
 
 import java.time.Instant
 
-data class PageRecognizedEvent (
-    val uploadUuid: String,
-    val pageUuid: String,
-    val userId: String,
-    val pageIndex: Int?,
-    val qrUuid: String?,
-    val targetLessonId: String?,
-    val uploadedTimestamp: Instant?
+data class PageRecognizedEvent(
+        val uploadUuid: String,
+        val pageUuid: String,
+        val userId: String,
+        val pageIndex: Int?,
+        val qrUuid: String?,
+        val targetLessonId: String?,
+        val uploadedTimestamp: Instant?
 )
