31fa5d06bef19c63f393f526073a6a4c8963bb49
Dmitry Romanov <me@wutiarn.ru>
2016-12-12 09:39:04 +0300

    EdustorAccount.assertScopeContains vararg

--- auth/src/main/kotlin/ru/edustor/commons/auth/util.kt
+++ auth/src/main/kotlin/ru/edustor/commons/auth/util.kt
@@ -3,8 +3,13 @@ package ru.edustor.commons.auth
 import ru.edustor.commons.auth.exception.ForbiddenException
 import ru.edustor.commons.models.internal.accounts.EdustorAccount
 
-fun EdustorAccount.assertScopeContains(requiredScope: String) {
-    if (this.activeToken != null && requiredScope !in this.activeToken!!.scope) {
+fun EdustorAccount.assertScopeContains(vararg requiredScope: String) {
+    this.activeToken ?: let {
+        throw ForbiddenException("This operation requires active token")
+    }
+
+    val missingScope = requiredScope.subtract(this.activeToken!!.scope.asIterable())
+    if (missingScope.isNotEmpty()) {
         throw ForbiddenException("This operation requires '$requiredScope' scope")
     }
 }
