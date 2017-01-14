a60e46d612d909f40cc81322e0e7a101e3d560bc
Dmitry Romanov <me@wutiarn.ru>
2017-01-14 14:34:48 +0300

    BinaryObjectStorageService.has method

--- storage/src/main/kotlin/ru/edustor/commons/storage/service/BinaryObjectStorageService.kt
+++ storage/src/main/kotlin/ru/edustor/commons/storage/service/BinaryObjectStorageService.kt
@@ -1,7 +1,9 @@
 package ru.edustor.commons.storage.service
 
+import io.minio.ErrorCode
 import io.minio.MinioClient
 import io.minio.errors.ErrorResponseException
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.io.InputStream
@@ -17,10 +19,10 @@ open class BinaryObjectStorageService(
         PAGE("edustor-pages", "pdf", "application/pdf")
     }
 
-    private val minio: MinioClient
+    private val minio: MinioClient = MinioClient(url, accessKey, secretKey)
+    private val logger = LoggerFactory.getLogger(BinaryObjectStorageService::class.java)
 
     init {
-        minio = MinioClient(url, accessKey, secretKey)
 
         ObjectType.values().forEach { type ->
             if (!minio.bucketExists(type.bucket)) {
@@ -43,5 +45,18 @@ open class BinaryObjectStorageService(
 
     open fun delete(type: ObjectType, id: String) {
         minio.removeObject(type.bucket, "$id.${type.extension}")
+    }
+
+    open fun has(type: ObjectType, id: String): Boolean {
+        try {
+            minio.statObject(type.bucket, "$id.${type.extension}")
+        } catch (e: ErrorResponseException) {
+            if (e.errorResponse().errorCode() == ErrorCode.NO_SUCH_OBJECT) {
+                return false
+            } else {
+                throw e
+            }
+        }
+        return true
     }
 }
