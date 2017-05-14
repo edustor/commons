ab46d4772f071c31bcae59abc14f30da3847a9fa
Dmitry Romanov <me@wutiarn.ru>
2017-05-14 11:32:43 +0300

    Use edustor.auth.jwk-pub-key env

--- auth/src/main/kotlin/ru/edustor/commons/auth/EdustorTokenValidator.kt
+++ auth/src/main/kotlin/ru/edustor/commons/auth/EdustorTokenValidator.kt
@@ -1,21 +1,33 @@
 package ru.edustor.commons.auth
 
 import io.jsonwebtoken.Jwts
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import org.bouncycastle.openssl.PEMParser
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter
+import org.springframework.core.env.Environment
 import org.springframework.stereotype.Component
 import ru.edustor.commons.auth.model.EdustorAuthProfile
-import java.security.KeyFactory
 import java.security.PublicKey
-import java.security.spec.X509EncodedKeySpec
+import java.security.Security
+import java.util.*
 
 @Component
-open class EdustorTokenValidator {
+open class EdustorTokenValidator(environment: Environment) {
 
     val publicKey: PublicKey
 
     init {
-        val keyBytes = javaClass.classLoader.getResourceAsStream("jwk.pub.der").readBytes()
-        val spec = X509EncodedKeySpec(keyBytes)
-        publicKey = KeyFactory.getInstance("RSA").generatePublic(spec)
+        val pemKeyBase64 = environment.getRequiredProperty("edustor.auth.jwk-pub-key")
+        val pemKey = Base64.getDecoder().decode(pemKeyBase64)
+
+        val result = PEMParser(pemKey.inputStream().reader()).readObject()
+        val pemObject = result as? SubjectPublicKeyInfo
+                ?: throw IllegalStateException("edustor.auth.jwk-pub-key must be base64-encoded public PEM file")
+
+        val converter = JcaPEMKeyConverter().setProvider("BC")
+        Security.addProvider(BouncyCastleProvider())
+        publicKey = converter.getPublicKey(pemObject)
     }
 
     fun validate(token: String): EdustorAuthProfile {
