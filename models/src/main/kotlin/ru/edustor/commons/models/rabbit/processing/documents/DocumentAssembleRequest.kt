b2fb8346a9cccd2f4a66ca24e424ee684a2a8951
Dmitry Romanov <me@wutiarn.ru>
2017-05-15 14:55:01 +0300

    DocumentAssembleRequest.timestamp

--- models/src/main/kotlin/ru/edustor/commons/models/rabbit/processing/documents/DocumentAssembleRequest.kt
+++ models/src/main/kotlin/ru/edustor/commons/models/rabbit/processing/documents/DocumentAssembleRequest.kt
@@ -1,10 +1,12 @@
 package ru.edustor.commons.models.rabbit.processing.documents
 
+import java.time.Instant
 import java.util.*
 
 data class DocumentAssembleRequest(
         val documentId: String,
         val pages: List<Page>,
+        val timestamp: Instant = Instant.now(),
         val requestId: String = UUID.randomUUID().toString()
         ) {
     data class Page(
